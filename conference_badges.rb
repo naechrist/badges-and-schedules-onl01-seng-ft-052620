0638be0a58555692414e73ddc2750b1a4e2e60fa
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-28 20:39:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
-
+puts badge_maker
 def batch_badge_creator(array)
   
 end
