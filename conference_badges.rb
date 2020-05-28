f06fcbef010d5797f2e6e687c9b1ae3ec808dab1
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-28 22:18:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,6 @@ attendees.collect { |attendee| badge_maker(attendee) }
 end
 
 def assign_rooms(attendees)
-  attendees.collect { |attendee| 
+  attendees.each_with_index.collect { |attendee, index| "Hello, #{attendee}! You'll be assigned to room #{index}!""}
+
 end
