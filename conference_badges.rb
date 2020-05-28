63d3405992cbbddbdbbf494d54cb6c3f1b6f0e47
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-28 20:58:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,6 @@ def badge_maker(name)
 end
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(attendees)
-  batch_badge_creator.list = badges
+  batch_badge_creator = badges
+  batch_badge_creator.list 
 end
